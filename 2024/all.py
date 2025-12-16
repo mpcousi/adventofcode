96836119212c464e8334cc596e0242631d562301
Martin Cousineau <martin.cousineau@cse-cst.gc.ca>
2025-12-16 15:29:13 -0500

    Small improvements to 2024

--- 2024/all.py
+++ 2024/all.py
@@ -1,11 +1,12 @@
 import subprocess
+import sys
 import time
 
 global_start_time = time.time()
 
 for i in range(1, 26):
     start_time = time.time()
-    subprocess.run(["python", f"day{i}.py"], capture_output=True)
+    subprocess.run([sys.executable, f"day{str(i).zfill(2)}.py"], capture_output=True)
     elapsed_time = time.time() - start_time
     print(f"Day {i}: {str(elapsed_time)[:10]} seconds")
 
